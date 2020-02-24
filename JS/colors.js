404a1ffd57d6dddf33fa36c80bf33e462c66c388
y0rfa1se <neargul@naver.com>
2020-02-25 00:11:31 +0900

    newpage

---
+++ JS/colors.js
@@ -0,0 +1,26 @@
+function nightday(self) {
+  var target = document.querySelector('body')
+  if (self.value === 'night') {
+    target.style.backgroundColor = 'black';
+    target.style.color = 'white';
+    self.value = 'day';
+
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = 'red';
+      i += 1;
+    }
+  } else {
+    target.style.backgroundColor = 'white';
+    target.style.color = 'black';
+    self.value = 'night';
+
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = 'rgb(63, 219, 177)';
+      i += 1;
+    }
+  }
+}
---
+++ typing/t.js
@@ -0,0 +1,4 @@
+function enter(id) {
+  var val = document.getElementById(id).value;
+  alert(val);
+}
